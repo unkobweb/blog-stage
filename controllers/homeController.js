bc887e22f40a86110162d8b61baf79442c6c6c3b
Kobweb <sieg.alexandre@gmail.com>
2020-07-23 16:03:12 +0200

    Add homeController

---
+++ controllers/homeController.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const mysql = require("mysql2");
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+});
+
+function index(req, res){
+    res.sendFile(path.join(__dirname, "../views","index.html"));
+}
+
+function allExperiences(req, res){
+    connection.query("SELECT * FROM experiences", function(err, results, fields) {
+        res.send(results);
+    })
+}
+
+module.exports = {index, allExperiences}
--- routes/web.js
+++ routes/web.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
+const homeController = require("../controllers/homeController");
+
+router.get("/", homeController.index);
+router.get("/allExperiences", homeController.allExperiences)
+
 module.exports = router;
