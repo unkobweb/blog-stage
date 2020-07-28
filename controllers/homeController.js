9fbcf5cc3961f3e9834c72d77b8da00aca9d02f4
Kobweb <sieg.alexandre@gmail.com>
2020-07-28 23:25:18 +0200

    Just pretty extension using double quote ^^

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,28 +1,28 @@
 const path = require("path");
 const mysql = require("mysql2");
-require('dotenv').config();
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 });
 
-function index(req, res){
-    res.sendFile(path.join(__dirname, "../views","index.html"));
+function index(req, res) {
+  res.sendFile(path.join(__dirname, "../views", "index.html"));
 }
 
-function experiencePage(req, res){
-    res.sendFile(path.join(__dirname, "../views","experience.html"));
+function experiencePage(req, res) {
+  res.sendFile(path.join(__dirname, "../views", "experience.html"));
 }
 
-function connectionPage(req, res){
-    res.sendFile(path.join(__dirname, '../views', "connexion.html"));
+function connectionPage(req, res) {
+  res.sendFile(path.join(__dirname, "../views", "connexion.html"));
 }
 
-function connect(req, res){
-    console.log(req.body);
+function connect(req, res) {
+  console.log(req.body);
 }
 
-module.exports = {index, experiencePage, connectionPage, connect}
+module.exports = { index, experiencePage, connectionPage, connect };
