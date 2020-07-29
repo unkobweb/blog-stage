1ba74d5188008e9888bd09aa6d2bda7a0a5fe6ee
Kobweb <sieg.alexandre@gmail.com>
2020-07-29 16:59:30 +0200

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -6,13 +6,21 @@ const adminController = require("../controllers/adminController");
 const apiController = require("../controllers/apiController");
 
 // WEB ROUTES
+router.use("/", (req, res, next) => {
+    console.log(req.session.user);
+    next();
+})
 router.get("/", homeController.index);
 router.get("/experience/:id", homeController.experiencePage);
 router.get("/connexion", adminController.connectionPage);
 router.post("/connect", adminController.connect);
+router.get("/admin", adminController.adminPage);
+router.get("/addChapter/:id", adminController.addChapterPage);
+router.post("/createChapter", adminController.createChapter);
 
 //API
 router.get("/api/allExperiences", apiController.allExperiences);
 router.get("/api/experience/:id", apiController.experience);
+router.get("/api/getAdmin", apiController.getAllChaptersAndExperiences);
 
 module.exports = router;
