821ad72403fb6a24de64ad7c97da56ab6bb459a7
Kobweb <sieg.alexandre@gmail.com>
2020-07-29 16:59:26 +0200

    Update connexion.jsx

--- public/js/connexion.jsx
+++ public/js/connexion.jsx
@@ -6,8 +6,9 @@ import '../css/dist/connexion.css'
 class Connexion extends React.Component{
     constructor(props){
         super(props);
-        this.state = {username: "", password: ""};
+        this.state = {username: "", password: "", message: {}};
         this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
 
@@ -20,30 +21,50 @@ class Connexion extends React.Component{
 
     handleClick(e) {
         e.preventDefault();
-        console.log(this.state);
+        this.setState({message: {}});
         fetch("/connect", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(this.state),
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password,
+            }),
+        }).then(res => res.json()).then(data => {
+            console.log(data)
+            this.setState({message: data});
+            if (data.type == "success"){
+                setTimeout(() => {
+                    window.location.href = "/admin";
+                }, 1000);
+            }
         });
     }
 
     render(){
+        let message = (<div></div>);
+        if (this.state.message.content !== undefined){
+            let type = "alert alert-"+this.state.message.type;
+            let content = this.state.message.content;
+            message = (<div className={type} role="alert">
+                {content}
+            </div>)
+        }
         return(
             <div className="container">
                 <h1>CONNEXION</h1>
+                {message}
                 <form>
                     <div className="form-group">
                         <label htmlFor="formGroupExampleInput">Identifiant</label>
-                        <input type="text" className="form-control" id="formGroupExampleInput" name="identifiant" placeholder="exemple@domaine.fr" value={this.state.mail} onChange={this.onChangeHandler}/>
+                        <input type="text" className="form-control" id="formGroupExampleInput" name="username" value={this.state.mail} onChange={this.onChangeHandler}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="formGroupExampleInput2">Mot de passe</label>
-                        <input type="text" className="form-control" id="formGroupExampleInput2" name="password" placeholder="Password" value={this.state.password} onChange={this.onChangeHandler}/>
+                        <input type="password" className="form-control" id="formGroupExampleInput2" name="password" value={this.state.password} onChange={this.onChangeHandler}/>
                     </div>
-                    <button className="btn btn-danger">Se connecter</button>
+                    <button className="btn btn-danger" onClick={this.handleClick}>Se connecter</button>
                 </form>
             </div>
         )
