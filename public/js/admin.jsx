256cac69af548d2ffd83975ed52e95ef50dbf368
Kobweb <sieg.alexandre@gmail.com>
2020-08-06 16:29:45 +0200

    Add moveChapter function and pass it to Chapter component

--- public/js/admin.jsx
+++ public/js/admin.jsx
@@ -8,16 +8,16 @@ function AddChapter({id}){
     )
 }
 
-function Chapitre({id, title}){
+function Chapitre({id, title, moveChapter, experience}){
     return(
         <div className="chapter">
             <span id="left">
                 <span id="arrows">
                     <div>
-                        <i className="fas fa-chevron-up"></i>
+                        <i onClick={moveChapter.bind(this, id, "up", experience.id)} className="fas fa-chevron-up"></i>
                     </div>
                     <div>
-                        <i className="fas fa-chevron-down"></i>
+                        <i onClick={moveChapter.bind(this, id, "down", experience.id)} className="fas fa-chevron-down"></i>
                     </div>
                 </span>
                 <span id="content">ID : {id} - {title}</span>
@@ -30,11 +30,11 @@ function Chapitre({id, title}){
     )
 }
 
-function Experience({experience}){
+function Experience({experience, moveChapter}){
     let rows = []
     let lastId = 0;
     experience.chapters.forEach(chapter => {
-        rows.push(<Chapitre key={chapter.id} id={chapter.id} title={chapter.title}/>)
+        rows.push(<Chapitre key={chapter.id} id={chapter.id} title={chapter.title} moveChapter={moveChapter} experience={experience}/>)
         lastId = chapter.id;
     })
     rows.push(<AddChapter key={lastId+1} id={experience.id}/>)
@@ -52,10 +52,26 @@ class Admin extends React.Component{
     constructor(props){
         super(props);
         this.state = {experiences : null}
+        this.moveChapter = this.moveChapter.bind(this);
     }
 
     componentDidMount(){
         fetch("/api/getAdmin").then(res => res.json()).then(data => this.setState({experiences: data}))
+    }
+
+    moveChapter(id, way, expId){
+        console.log(id + " of "+expId+" exp want to go "+way)
+        fetch("/moveChapter",{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                experienceId: expId,
+                chapterId: id,
+                way: way
+            })
+        })
     }
 
     render(){
@@ -64,7 +80,7 @@ class Admin extends React.Component{
         }
         let rows = [];
         this.state.experiences.forEach(experience => {
-            rows.push(<Experience key={experience.id} experience={experience}/>)
+            rows.push(<Experience key={experience.id} experience={experience} moveChapter={this.moveChapter}/>)
         });
         return(
             <div>
