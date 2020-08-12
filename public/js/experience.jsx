bd32f429bab5b5030bc7b4a895f38cab818fd5e6
Kobweb <sieg.alexandre@gmail.com>
2020-08-12 23:15:42 +0200

    Update experience.jsx

--- public/js/experience.jsx
+++ public/js/experience.jsx
@@ -1,70 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../css/experience.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../css/experience.scss";
 
-function Title({title, slug, mainColor}){
-    return(
-        <div id="title">
-            <div id="image" style={{backgroundImage: "url('/img/"+slug+".png')"}}></div>
-            <h1 style={{backgroundColor: mainColor}}>{title}</h1>
-        </div>
-    )
+function Title({ title, slug, mainColor }) {
+  return (
+    <div id="title">
+      <div
+        id="image"
+        style={{ backgroundImage: "url('/img/" + slug + ".png')" }}
+      ></div>
+      <h1 style={{ backgroundColor: mainColor }}>{title}</h1>
+    </div>
+  );
 }
 
-function Chapitres({mainColor, children}){
-    return (
+function Chapitres({ mainColor, children }) {
+  return (
+    <div>
+      <h2 style={{ color: mainColor, textAlign: "center" }}>Chapitres</h2>
+      <div id="chapters">{children}</div>
+    </div>
+  );
+}
+
+function Chapitre({ slug, title, number }) {
+  return (
+    <div
+      className="chapter"
+      style={{ backgroundImage: "url('/img/" + slug + ".png')" }}
+    >
+      <h3>
+        Chapitre {number} - {title}
+      </h3>
+    </div>
+  );
+}
+
+class Experience extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { experience: null };
+  }
+
+  componentDidMount() {
+    let link = window.location.href.split("/");
+    let id = link[link.length - 1];
+    fetch("http://localhost:8000/api/experience/" + id)
+      .then((data) => data.json())
+      .then((result) => {
+        console.log(result);
+        this.setState({ experience: result });
+        document.title = result.company;
+      });
+  }
+
+  render() {
+    if (this.state.experience != null) {
+      let {
+        id,
+        company,
+        slug,
+        main_color,
+        contract,
+        job,
+        period,
+        second_color,
+        chapters,
+      } = this.state.experience;
+      console.log(chapters);
+      let rows = [];
+      chapters.forEach((chapter) => {
+        rows.push(
+          <Chapitre
+            key={chapter.id}
+            slug={chapter.slug}
+            title={chapter.title}
+            number={chapter.number}
+          />
+        );
+      });
+      return (
         <div>
-            <h2 style={{color: mainColor, textAlign: "center"}}>Chapitres</h2>
-            <div id="chapters">
-                {children}
-            </div>
+          <Title title={company} slug={slug} mainColor={main_color} />
+          <Chapitres>{rows}</Chapitres>
         </div>
-    )
+      );
+    }
+    return <div></div>;
+  }
 }
 
-function Chapitre({slug, title}){
-    return(
-        <div className="chapter" style={{backgroundImage: "url('/img/"+slug+".png')"}}>
-            <h3>{title}</h3>
-        </div>
-    )
-}
-
-class Experience extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {experience: null};
-    }
-
-    componentDidMount(){
-        let link = window.location.href.split("/");
-        let id = link[link.length-1];
-        fetch("http://localhost:8000/api/experience/"+id).then(data => data.json()).then(result => {
-            console.log(result);
-            this.setState({experience : result});
-            document.title = result.company;
-        });
-    }
-
-    render(){
-        if (this.state.experience != null){
-            let {id, company, slug, main_color, contract, job, period, second_color, chapters} = this.state.experience;
-            console.log(chapters)
-            let rows = [];
-            chapters.forEach(chapter => {
-                rows.push(<Chapitre key={chapter.id} slug={chapter.slug} title={chapter.title}/>);
-            })
-            return(
-            <div>
-                <Title title={company} slug={slug} mainColor={main_color}/>
-                <Chapitres>{rows}</Chapitres>
-            </div>
-            )
-        }
-        return (
-            <div></div>
-        )
-    }
-}
-
-ReactDOM.render(<Experience/>, document.querySelector("#app"));
+ReactDOM.render(<Experience />, document.querySelector("#app"));
