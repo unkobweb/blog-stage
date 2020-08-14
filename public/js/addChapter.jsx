fbc6a229c6257b199e3d7be76bf0f5bc40f86190
Kobweb <sieg.alexandre@gmail.com>
2020-08-14 15:19:33 +0200

    Update addChapter.jsx

--- public/js/addChapter.jsx
+++ public/js/addChapter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ChapterInputs from './chapterInputs';
-import '../css/addChapter.scss'
+import '../css/chapterInputs.scss'
 
 
 
