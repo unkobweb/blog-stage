9798bd9c133d9df16a0a25582c0e726ff2a90d82
Kobweb <sieg.alexandre@gmail.com>
2020-08-14 15:19:29 +0200

    Update admin.jsx

--- public/js/admin.jsx
+++ public/js/admin.jsx
@@ -8,7 +8,7 @@ function AddChapter({id}){
     )
 }
 
-function Chapitre({number, title, moveChapter, experience}){
+function Chapitre({id, number, title, moveChapter, deleteChapter, experience}){
     return(
         <div className="chapter">
             <span id="left">
@@ -20,24 +20,24 @@ function Chapitre({number, title, moveChapter, experience}){
                         <i onClick={moveChapter.bind(this, number, "down", experience.id)} className="fas fa-chevron-down"></i>
                     </div>
                 </span>
-                <span id="content">ID : {number} - {title}</span>
+                <span id="content">Chapitre {number} - {title}</span>
             </span>
             <span id="right">
-                <button>Modifier</button>
-                <button>Supprimer</button>
+                <a href={"/modifyChapter/"+id}>Modifier</a>
+                <a onClick={deleteChapter.bind(this,id)}>Supprimer</a>
             </span>
         </div>
     )
 }
 
-function Experience({experience, moveChapter}){
+function Experience({experience, moveChapter, deleteChapter}){
     let rows = []
     let maxId = 0;
     experience.chapters.forEach(chapter => {
-        rows.push(<Chapitre key={chapter.id} number={chapter.number} title={chapter.title} moveChapter={moveChapter} experience={experience}/>)
+        rows.push(<Chapitre key={chapter.id} id={chapter.id} number={chapter.number} title={chapter.title} moveChapter={moveChapter} deleteChapter={deleteChapter} experience={experience}/>)
         maxId = chapter.id > maxId ? chapter.id : maxId;
     })
-    rows.push(<AddChapter key={maxId+1} number={experience.number}/>)
+    rows.push(<AddChapter key={maxId+1} id={experience.id}/>)
     return(
         <div className="experience">
             <h2>{experience.company}</h2>
@@ -53,6 +53,7 @@ class Admin extends React.Component{
         super(props);
         this.state = {experiences : null}
         this.moveChapter = this.moveChapter.bind(this);
+        this.deleteChapter = this.deleteChapter.bind(this);
     }
 
     componentDidMount(){
@@ -79,13 +80,23 @@ class Admin extends React.Component{
         });
     }
 
+    deleteChapter(id){
+        if (window.confirm("Voulez vous vraiment supprimer la chapitre d'id "+id)){
+            fetch("/deleteChapter/"+id,{
+                method: "DELETE"
+            }).then(()=>{
+                this.componentDidMount();
+            })
+        }
+    }
+
     render(){
         if (this.state.experiences == null){
             return <div></div>
         }
         let rows = [];
         this.state.experiences.forEach(experience => {
-            rows.push(<Experience key={experience.id} experience={experience} moveChapter={this.moveChapter}/>)
+            rows.push(<Experience key={experience.id} experience={experience} moveChapter={this.moveChapter} deleteChapter={this.deleteChapter}/>)
         });
         return(
             <div>
