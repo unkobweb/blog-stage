9fca4345d82f57fa7d0f03ea51ed70b658d96d5b
Kobweb <sieg.alexandre@gmail.com>
2020-08-27 14:09:08 +0200

    Update index.jsx

--- public/js/index.jsx
+++ public/js/index.jsx
@@ -1,59 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../css/index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../css/index.scss";
 
-function Separator(){
-    return(
-        <div id="separator"></div>
-    )
+function Header() {
+  return (
+    <div id="header">
+      <h1>Alexandre Sieg</h1>
+    </div>
+  );
 }
 
-function Experience({experience}){
-    let {id, slug, main_color, period, company, job} = experience
+function Buttons() {
+  return (
+    <div id="buttons">
+      <div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="/documents/CV_Alexandre_SIEG.pdf"
+        >
+          CV Developpeur
+        </a>
+      </div>
+      <div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="/documents/CV_Sieg_Alexandre.pdf"
+        >
+          CV Système / Réseau
+        </a>
+      </div>
+    </div>
+  );
+}
 
-    function handleClick(e){
-        window.location.href = "/experience/"+id
-    }
+function Experience({ experience }) {
+  let { id, slug, main_color, period, company, job } = experience;
 
-    return(
-        <div className="experience" data-id={id} style={{backgroundImage: "url('/img/"+slug+".png')"}} onClick={handleClick}>
-            <h3 className="period">{period}</h3>
-            <h3 className="name">{company}</h3>
-            <h4 className="job">{job}</h4>
-            <div className="color-overlay"></div>
+  function handleClick(e) {
+    window.location.href = "/experience/" + id;
+  }
+
+  return (
+    <div
+      className="experience"
+      data-id={id}
+      style={{ backgroundImage: "url('/img/" + slug + ".png')" }}
+      onClick={handleClick}
+    >
+      <h3 className="period">{period}</h3>
+      <h3 className="name">{company}</h3>
+      <h4 className="job">{job}</h4>
+      <div className="color-overlay"></div>
+    </div>
+  );
+}
+
+class Experiences extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { experiences: null };
+  }
+
+  componentDidMount() {
+    fetch("/api/allExperiences")
+      .then((data) => data.json())
+      .then((result) => {
+        this.setState({ experiences: result });
+      });
+  }
+
+  render() {
+    const rows = [];
+    if (this.state.experiences != null) {
+      this.state.experiences.forEach((experience) => {
+        rows.push(<Experience key={experience.id} experience={experience} />);
+      });
+      return (
+        <div>
+          <Header />
+          <div id="experiences">{rows}</div>
+          <Buttons />
         </div>
-    )
+      );
+    }
+    return <div></div>;
+  }
 }
 
-class Experiences extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.state = {experiences: null};
-    }
-
-    componentDidMount(){
-        fetch("/api/allExperiences").then(data => data.json()).then(result => {
-            this.setState({experiences : result});
-        });
-    }
-
-    render(){
-        const rows = [];
-        if (this.state.experiences != null){
-            this.state.experiences.forEach(experience => {
-                rows.push(<Experience key={experience.id} experience={experience}/>);
-            });
-            return (
-                <div>
-                    <div id="experiences">{rows}</div>
-                    <Separator/>
-                </div>
-            )
-        }
-        return <div></div>
-    }
-
-}
-
-ReactDOM.render(<Experiences/>,document.querySelector("#app"));
+ReactDOM.render(<Experiences />, document.querySelector("#app"));
